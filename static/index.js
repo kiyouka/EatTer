688a9b7361ca0c8ff010b72e3c28dc8f7a894c75
FeletexGee <69303837+FeletexGee@users.noreply.github.com>
2022-01-24 19:07:27 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '咱可得练一练啊！';
+    if (score <= 99) return '咱这可得加油啊！';
+    if (score <= 149) return '还不戳~';
+    if (score <= 199) return 'Your answer!';
     return '人？';
 }
 
